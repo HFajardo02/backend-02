8c8b36ee41a6b85ce06df7cd4cbc462f2571480f
Hector <hafr40@gmail.com>
2022-11-18 11:44:26 -0600

    productmanager

--- desafios/productmanager.js
+++ desafios/productmanager.js
@@ -1,9 +1,42 @@
 class ProductManager {
 
-    constructor{
+    constructor () {
         this.products = []
     }
 
-    
+    getProducts = () => {return this.products}
 
+    getProductById = () => {
+        const count = this.products.length
+        if (count == 0) return 1
+
+        const lastProduct = this.products[count-1]
+        const lastID = lastProduct.id
+        const nextID = lastID + 1
+        // const nextID = (amount > 0) ? this.products[amount - 1].id + 1 : 1; 
+
+        return nextID
+    }
+
+    addProduct = (title, description, price, thumbnail, code, stock) => {
+        const id = this.getProductById()
+    
+        const product = {
+            id,
+            title, //(nombre del producto)
+            description, //(descripción del producto)
+            price, //(precio)
+            thumbnail, //(ruta de imagen)
+            code, //(código identificador)
+            stock //(número de piezas disponibles)
+        }
+
+        this.products.push(product)
+    }
 }
+
+const manager = new ProductManager()
+manager.addProduct("Producto1", "DescProducto1", 100, null, "CodeProducto1", 100)
+manager.addProduct("Producto2", "DescProducto2", 200, null, "CodeProducto2", 200)
+
+console.log(manager.products);
